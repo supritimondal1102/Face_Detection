75e725fc2e2add1d7304328d9bab2f75d23b6db9
Supriti Mondal <165676092+supritimondal1102@users.noreply.github.com>
2024-07-13 23:13:53 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+import cv2
+face_cap = cv2.CascadeClassifier("C:/Users/supri/Desktop/haarcascade_frontalface_default.xml")
+video_cap = cv2.VideoCapture(0)
+while True:
+    ret, video_data = video_cap.read() 
+    color = cv2.cvtColor(video_data,cv2.COLOR_BGR2GRAY)
+    faces = face_cap.detectMultiScale(
+        color,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30,30),  
+        flags = cv2.CASCADE_SCALE_IMAGE
+    )
+    for (x,y,w,h) in faces:
+        cv2.rectangle(video_data,(x, y),(x+w, y+h),(0,255,0),2)
+    cv2.imshow("video_live",video_data) 
+    if cv2.waitKey(10) == ord("a"):
+        break
+video_cap.release() 
+ 
+#video_cap = cv2.VideoCapture(0)
+#while True :
+ #   ret , video = video_cap.read() 
+  #  cv2.imshow("video_live",video_cap) 
+   # if cv2.waitkey(10) == ord("a"):
+    #    break
+    #video_cap.release() 
+
